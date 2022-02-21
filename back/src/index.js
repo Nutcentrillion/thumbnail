5e44e89c7e4f41c7b6f87a7d1879709a03b6447a
Nutcentrillion <nattanun.ch@centrilliontech.co.th>
2022-02-21 10:42:11 +0700

    update

--- back/src/index.js
+++ back/src/index.js
@@ -1,50 +1,56 @@
 const express = require("express");
 const cors = require("cors");
-const formidable = require("formidable");
-const fs = require("fs");
 const multer = require("multer");
+const axios = require("axios");
+const FormData = require("form-data");
+const fs = require("fs");
+const path = require("path");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+// app.use(fileUpload()); ===> req.files
 
-app.post("/", (req, res) => {
-  const form = formidable({ multiples: true });
+const storage = multer.diskStorage({
+  destination: function (req, file, callback) {
+    callback(null, "./public");
+  },
+  filename: function (req, file, callback) {
+    callback(null, file.originalname);
+  },
+});
+const upload = multer({ storage }).single("file");
 
-  form.parse(req, (err, fields, files) => {
-    // const extension = files.file.originalFilename.split(".").pop();
+app.post("/", upload, async function (req, res) {
+  const formData = new FormData();
 
-    fs.readFile(files.file.filepath, "base64", (error, data) => {
-      console.log(files.file);
-      if (error) console.log(error);
+  formData.append(
+    "file",
+    fs.createReadStream(
+      path.join(process.cwd(), `./public/${req.file.originalname}`)
+    )
+  );
 
-      return res.json({ data });
-    });
+  const response = await axios.post(
+    "http://localhost:8000/preview/1024x1024",
+    formData,
+    {
+      headers: formData.getHeaders(),
+      responseType: "arraybuffer",
+    }
+  );
+
+  let base64ImageString = Buffer.from(response.data, "binary").toString(
+    "base64"
+  );
+
+  res.json({ data: base64ImageString });
+
+  fs.unlink(req.file.path, (err) => {
+    console.log(err);
   });
 });
-
-// const storage = multer.diskStorage({
-//   destination: function (req, file, callback) {
-//     callback(null, "./public");
-//   },
-//   filename: function (req, file, callback) {
-//     callback(null, file.originalname);
-//   },
-// });
-// const upload = multer({ storage }).single("file");
-
-// app.post("/", upload, async function (req, res) {
-//   fs.readFile(req.file.path, "utf8", (_, data) => {
-//     return res.json({ data: data });
-//   });
-
-//   console.log(req.file);
-
-//   // fs.unlink(req.file.path, (err) => {
-//   //   console.log(err);
-//   // });
-// });
 
 app.listen(5000, () => {
   console.log("http://localhost:5000");
--- front/src/App.js
+++ front/src/App.js
@@ -4,39 +4,29 @@ import axios from "axios";
 
 function App() {
   const [thumbnail, setThumbnail] = useState(null);
-  // const [thumbnailReact, setThumbnailReact] = useState(null);
 
   const handleChange = async (e) => {
     const file = e.target.files[0];
 
-    // const reader = new FileReader();
-
-    // reader.addEventListener("load", (event) => {
-    //   setThumbnailReact(event.target.result);
-    // });
-
-    // reader.readAsDataURL(selectedFile);
-
     const formData = new FormData();
     formData.append("file", file);
+
     const response = await axios.post("http://localhost:5000", formData);
 
     setThumbnail(response.data.data);
   };
 
   console.log("thumbnail => ", thumbnail);
-  // console.log("thumbnailReact => ", thumbnailReact);
 
   return (
     <div className="App">
       <input type="file" onChange={handleChange} />
       <img
-        src={"data:image/jpeg;base64," + thumbnail}
-        alt=""
-        with={200}
-        height={200}
+        src={`data:image/jpeg;base64,${thumbnail}`}
+        with={300}
+        height={300}
+        alt="Thumbnail"
       />
-      {/* <div>{JSON.stringify(thumbnail)}</div> */}
     </div>
   );
 }
